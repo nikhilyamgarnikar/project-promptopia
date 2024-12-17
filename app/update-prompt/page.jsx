ff1b3c17224d2e720989ec4dc364964d77cc9c21
nikhilyamgarnikar <prathameshshingare99@gmail.com>
2024-12-17 19:20:24 +0530

    suspense added

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useState, Suspense} from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 
 import Form from "@components/Form";
@@ -63,4 +63,13 @@ const UpdatePrompt = () => {
   );
 };
 
-export default UpdatePrompt;
+
+const Page = () => {
+  return (
+    <Suspense>
+      <UpdatePrompt/>
+    </Suspense>
+  )
+}
+
+export default Page;
